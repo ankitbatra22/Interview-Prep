e111d55e851a3aeb903535b98c4486221a301928
ankitbatra2 <ankitbatra2202@gmail.com>
2021-01-08 02:32:18 -0500

    inital commit

--- Summer2020/125.py
+++ Summer2020/125.py
@@ -19,5 +19,23 @@ class Solution:
       return not stack
 
 
-"""dict = {'(':')', '{':'}', '[':']'}
-print(dict["{"])"""
+# -- Alternate --
+      def isValid(self, s: str) -> bool:
+        stack = []
+        key = {"]":"[", "}":"{", ")":"("}
+        
+        for char in s:
+            if char in key.values():
+                stack.append(char)
+            else:
+                if len(stack) > 0 and stack[-1] == key[char]:
+                    stack.pop()
+                else:
+                    return False
+        
+        if (len(stack) == 0):
+            return True
+
+
+
+
