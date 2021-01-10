271989f7275efa32cb3eb3911e1c6f66752c32a6
ankitbatra2 <ankitbatra2202@gmail.com>
2021-01-10 13:11:42 -0500

    latest

--- Summer2020/21.py
+++ Summer2020/21.py
@@ -8,3 +8,17 @@ class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         pre = ListNode(-1)
         prev = pre
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+                
+            prev = prev.next
+        
+        prev.next = l1 if l1 is not None else l2
+        
+        return pre.next
