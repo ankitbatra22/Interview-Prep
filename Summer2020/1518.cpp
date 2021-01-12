984c77d598f02b628fdfdb09b070f6cac6261851
ankitbatra2 <ankitbatra2202@gmail.com>
2021-01-12 03:02:51 -0500

    updated

---
+++ Summer2020/1518.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int totalBottles = 0;
+        int emptyBottles = 0;
+        
+        while (numBottles > 0) {
+            totalBottles += numBottles;
+            int consumed = numBottles;
+            numBottles /= numExchange;
+            emptyBottles += consumed % numExchange;
+            
+            if(emptyBottles >= numExchange){
+                numBottles+=(emptyBottles/numExchange);
+                emptyBottles=emptyBottles % numExchange;
+            }
+        }
+        return totalBottles;
+    }
+};
