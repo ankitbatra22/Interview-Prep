187c998d6898e398e32654f646d6385864162315
ankitbatra2 <ankitbatra2202@gmail.com>
2021-08-10 12:14:16 -0400

    151

---
+++ Winter2022/151.py
@@ -0,0 +1,103 @@
+# Method 1: Using built in split and join method O(n) space and time complexity
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        output = []
+        for word in words[::-1]:
+            output.append(word)
+        
+        return " ".join(output)
+
+# Method 2: 
+class Solution:
+    def trim_spaces(self, s: str) -> list:
+        left, right = 0, len(s) - 1
+        # remove leading spaces
+        while left <= right and s[left] == ' ':
+            left += 1
+        
+        # remove trailing spaces
+        while left <= right and s[right] == ' ':
+            right -= 1
+        
+        # reduce multiple spaces to single one
+        output = []
+        while left <= right:
+            if s[left] != ' ':
+                output.append(s[left])
+            elif output[-1] != ' ':
+                output.append(s[left])
+            left += 1
+        
+        return output
+            
+    def reverse(self, l: list, left: int, right: int) -> None:
+        while left < right:
+            l[left], l[right] = l[right], l[left]
+            left, right = left + 1, right - 1
+            
+    def reverse_each_word(self, l: list) -> None:
+        n = len(l)
+        start = end = 0
+        
+        while start < n:
+            # go to the end of the word
+            while end < n and l[end] != ' ':
+                end += 1
+            # reverse the word
+            self.reverse(l, start, end - 1)
+            # move to the next word
+            start = end + 1
+            end += 1
+                
+    def reverseWords(self, s: str) -> str:
+        # converst string to char array 
+        # and trim spaces at the same time
+        l = self.trim_spaces(s)
+        
+        # reverse the whole string
+        self.reverse(l, 0, len(l) - 1)
+        
+        # reverse each word
+        self.reverse_each_word(l)
+        
+        return ''.join(l)
+
+
+# simplified 
+
+s = "the sky is blue  "
+l = []
+l = trim_spaces(s)
+
+# O(n)
+def trim_spaces(s: str) -> list:
+    left, right = 0, len(s) - 1
+    # remove leading spaces
+    while left <= right and s[left] == ' ':
+        left += 1
+    
+    # remove trailing spaces
+    while left <= right and s[right] == ' ':
+        right -= 1
+    
+    # reduce multiple spaces to single one
+    output = []
+    while left <= right:
+        if s[left] != ' ':
+            output.append(s[left])
+        elif output[-1] != ' ':
+            output.append(s[left])
+        left += 1
+    
+    return output
+
+#(O(n/2))
+left, right = 0, len(l)-1
+while left <= right:
+    l[left], l[right] = l[right], l[left]
+    left += 1
+    right -= 1
+
+
