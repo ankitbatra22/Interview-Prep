600a74a41780dbe016a462da8a57e036fb7c5a08
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-03-04 00:34:04 -0500

    better solution

--- Fall2022/Easy/392.py
+++ Fall2022/Easy/392.py
@@ -1,13 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        seqIdx = 0
-        if s == t:
-            return True
-        for value in t:
-            if len(s) == seqIdx:
-                break
-            if value == s[seqIdx]:
-                seqIdx += 1
-        return seqIdx == len(s)
-
+        # want seq_idx == len(sequence)
+        arr_idx = 0
+        seq_idx = 0
+        while arr_idx < len(t) and seq_idx < len(s):
+            if t[arr_idx] == s[seq_idx]:
+                seq_idx+=1
+            arr_idx+=1
+        
+        return seq_idx == len(s)
+        
 # Runs in O(n) time and O(1) space.
