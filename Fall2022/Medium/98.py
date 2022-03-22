a8342328e04bdf7d0b763076c82577c867273389
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-03-22 13:13:21 -0400

    validate BST (98)

--- Fall2022/Medium/98.py
+++ Fall2022/Medium/98.py
@@ -17,4 +17,26 @@ class Solution:
 # O(n) time
 # Idea is to use DFS recursively and have left and right boundries. Node val should always be between left and right for it to be valid
 # Then recursively call the left and right subtree up to the left and right boundry accordingly.
+# When going left, update right boundry to curr node val. When going right, update left boundry to curr node val.
+
+# BST: 
+#       9
+#     /   \
+#    7    20
+#   / \  /  \
+#  2   8 15  22
+
+# -inf < 9 < inf
+
+# -inf < 7 < 9
+# -inf < 2 < 7
+# -inf < 8 < 7
+
+# 9 < 20 < inf
+# 9 < 15 < 20
+# 20 < 22 < inf
+
+
+
+
 
